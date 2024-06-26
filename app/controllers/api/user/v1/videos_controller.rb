3aed158273f3e2b3a044a21c0a11fe75222b099c
SJS-Crystal <48819487+SJS-Crystal@users.noreply.github.com>
2024-06-27 02:09:43 +0700

    Update videos_controller.rb

--- app/controllers/api/user/v1/videos_controller.rb
+++ app/controllers/api/user/v1/videos_controller.rb
@@ -1,7 +1,7 @@
 class Api::User::V1::VideosController < Api::User::V1::BaseController
   skip_before_action :authenticate, only: %i[index]
 
-  $user_desc << 'api/user/v1/videos | POST | Authorization(header), url | Share a video'
+  $user_desc << 'api/user/v1/videos | POST | Authorization(header), url | Share a video1'
   def create
     video_info = Youtube.get_video_info(params[:url])
     return render json: {message: 'Invalid youtube url'}, status: :bad_request unless video_info
